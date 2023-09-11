dace4ed6013d149c345198d047bbf4167fab8c9a
msadza <60848941+sadzamzeko@users.noreply.github.com>
2023-09-11 15:36:30 +0400

    change html and JS

--- main.js
+++ main.js
@@ -1,37 +1,14 @@
 const planets = document.querySelectorAll('.planet-link');
 const planetInfo = document.querySelector('.planet-info');
+const wikip = document.querySelector('.wikip');
 const planetHeaderName = document.querySelector('.planet-header-name');
+const rotTime = document.querySelector('.rot-time');
+const revTime = document.querySelector('.rev-time');
+const radius = document.querySelector('.radius');
+const temp = document.querySelector('.temp');
 
 const API_URL = 'https://planets-api.vercel.app/api/v1/planets';
 
-
-
-for(let i=0 ; i< planets.length; i++){
-    console.log(planets[i]);
-    console.log("mushoabs");
-    planets[i].addEventListener('click', () => {
-        console.log("esec mushaobs + ");
-        console.log(planets[i]);
-        console.log("esec  + ");
-        console.log(planets[i].innerText);
-    getPlanetHeader(planets[i].innerText);
-       console.log("esec2  + ");
-    })
-}
-// const API_URL = 'https://planets-api.vercel.app/api/v1/planets';
-const getPlanetHeader = async (planet =  'Mercury') => {
-    const response = await fetch(`https://planets-api.vercel.app/api/v1/planets/${planet}`);
-    const data = await response.json();
-    for(let i=0 ; i< planets.length; i++){
-        planetHeaderName.textContent = data.name;
-        console.log(data);
-        console.log('test3');
-    }
-    
-}
-getPlanetHeader('mercury');
-
-console.log('--------------------------------------------------------------')
 
 for(let j=0 ; j< planets.length; j++){
     console.log(planets[j]);
@@ -49,11 +26,21 @@ for(let j=0 ; j< planets.length; j++){
 const getPlanet = async (planet =  'Mercury') => {
     const response = await fetch(`https://planets-api.vercel.app/api/v1/planets/${planet}`);
     const data = await response.json();
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa1');
     for(let i=0 ; i< planets.length; i++){
+        console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa2');
+        console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa3');
         planetInfo.textContent = data.overview.content;
+        planetHeaderName.textContent = data.name;
+        wikip.textContent = data.structure.source;
+        rotTime.textContent= data.rotation;
+        revTime.textContent= data.revolution;
+        radius.textContent= data.radius;
+        temp.textContent= data.temperature;
         console.log(data);
         console.log('test3');
     }
     
 }
 getPlanet('mercury');
+
